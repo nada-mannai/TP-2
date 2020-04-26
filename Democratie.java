2c1fee38ac34d534321e89105324869e21eec5e0
nada-mannai <61947651+nada-mannai@users.noreply.github.com>
2020-04-27 00:11:41 +0200

    Add files via upload

---
+++ Democratie.java
@@ -0,0 +1,17 @@
+package Bean;
+
+public class Democratie {
+	private int voix;
+	public Democratie() {
+		voix=0;
+	}
+	public int getVoix() {
+		return voix;
+	}
+	public void setVoix(int voix) {
+		this.voix = voix;
+	}
+	public int voter(){
+		return voix++;
+	}
+}
